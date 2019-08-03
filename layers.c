ee858596aa075fd11a3ebadf1fea13ed1641da45
mikaelharseno <mikael_harseno@yahoo.com>
2019-08-03 13:02:21 -0700

    work?

--- layers.c
+++ layers.c
@@ -152,8 +152,10 @@ void conv_forward(conv_layer_t* l, volume_t** inputs, volume_t** outputs, int st
                   __m128d inm = _mm_load_pd((inw+((inwidth * in_y) + in_x) * indepth + fd));
                   __m128d mult = _mm_mul_pd(filterm, inm);
                   //total = temp;
-                  total = _mm_add_pd(total, mult);
-                  _mm_storeu_pd((double*) doublearray, (__m128d) total);
+                  //temp = _mm_add_pd(total, mult);
+                  _mm_storeu_pd((double*) doublearray, (__m128d) mult);
+                  sum = sum + doublearray[0];
+                  sum = sum + doublearray[1];
                 }
                 for (int fd = indepth/2*2; fd < indepth; fd++) {
                   sum += filtw[((filw * fy) + fx) * indepth + fd]
@@ -166,8 +168,8 @@ void conv_forward(conv_layer_t* l, volume_t** inputs, volume_t** outputs, int st
           //_mm_store_pd((double*) doublearray, (__m128d) total);
           //_mm_storeu_pd((double*) doublearray, (__m128d) total);
 
-          sum = sum + doublearray[0];
-          sum = sum + doublearray[1];
+          //sum = sum + doublearray[0];
+          //sum = sum + doublearray[1];
 
           outw[((outwidth * out_y) + out_x) * outdepth + f] = sum;
 
