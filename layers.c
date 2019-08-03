856cb1b4bfbc289963a0563667ec735fb7cec7b6
mikaelharseno <mikael_harseno@yahoo.com>
2019-08-03 14:19:51 -0700

    work?

--- layers.c
+++ layers.c
@@ -190,7 +190,7 @@ void conv_forward(conv_layer_t* l, volume_t** inputs, volume_t** outputs, int st
               }
               //return;
             }
-            return;
+            //return;
           }
 
           //_mm_store_pd(doublearray, total);
